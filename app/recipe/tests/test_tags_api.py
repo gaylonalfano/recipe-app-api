58125d4bb043bfbcdae75438dc48408514af099b
gaylonalfano <gaylon.alfano@gmail.com>
2019-02-23 10:23:49 +0800

    Added create tag feature by using ListModelMixin and CreateModelMixin. Also overrode perform_create() to create a new tag and assign to authenticated user.

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -92,6 +92,25 @@ class PrivateTagsApiTests(TestCase):
         # Assert that name of tag matches tag.name
         self.assertEqual(res.data[0]['name'], tag.name)
 
+    def test_create_tag_successful(self):
+        """Test creating a new tag"""
+        payload = {'name': 'Test tag'}
+        self.client.post(TAGS_URL, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating a new tag with invalid payload"""
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
 
 """
     This is what I attempted before instructor. READ NOTES!
